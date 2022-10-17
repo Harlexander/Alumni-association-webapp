8bcd9f1336fe7e06a88274bfbeb1926db7bfe3b6
harlexander <dunkwualex6@gmail.com>
2022-10-17 05:51:31 +0100

    Reveal animation

--- components/Responsibility/ResponsilbiltySection.js
+++ components/Responsibility/ResponsilbiltySection.js
@@ -3,32 +3,39 @@ import ResponsibilityContainer from './ResponsibilityContainer'
 import { Slide } from 'react-awesome-reveal'
 const ResponsilbiltySection = () => {
   return (
+    
     <div className="flex flex-col space-y-20 items-center justify-center  pt-20 pb-24 bg-[#FED253]">
-        <p className="text-4xl font-primary">Our Responsibility</p>
+        <Slide direction='up'><p className="text-4xl font-primary">Our Responsibility</p>
 
-    <Slide direction='up'>
-        <div className='md:flex gap-14 px-5 space-y-10 md:space-y-0 md:px-36'>
-        <ResponsibilityContainer
-        src={"apply.png"}
-        title="Scholarship and Empowerment"
-        content={"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,"}/>
-        <ResponsibilityContainer
-        src={"awards.png"}
-        title="Help Current Students/Awards"
-        content={"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,"}/>
-        <ResponsibilityContainer
-        src={"dues.png"}
-        title="Subscription and Dues"
-        content={"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,"}/>
-        <ResponsibilityContainer
-        src={"community.png"}
-        title="Grow Our Community"
-        content={"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,"}/>
-        </div>      
-    </Slide>
-
-        
+          <div className='md:flex gap-14 px-5 space-y-10 md:space-y-0 md:px-36'>
+          {/* <Slide direction='up'> */}
+          <ResponsibilityContainer
+          src={"apply.png"}
+          title="Scholarship and Empowerment"
+          content={"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,"}/>
+          {/* </Slide> */}
+          {/* <Slide direction='up'> */}
+          <ResponsibilityContainer
+          src={"awards.png"}
+          title="Help Current Students/Awards"
+          content={"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,"}/>
+          {/* </Slide> */}
+          {/* <Slide direction='up'> */}
+          <ResponsibilityContainer
+          src={"dues.png"}
+          title="Subscription and Dues"
+          content={"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,"}/>
+          {/* </Slide>
+          <Slide direction='up'> */}
+          <ResponsibilityContainer
+          src={"community.png"}
+          title="Grow Our Community"
+          content={"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,"}/>
+          {/* </Slide> */}
+          </div>
+        </Slide>     
     </div>
+    
   )
 }
 
